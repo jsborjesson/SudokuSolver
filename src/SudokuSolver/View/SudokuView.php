d5df054f9f71eb3564ff1ff1d428c1e426d29190
alcesleo <lagginglion@gmail.com>
2013-10-16 12:38:23 +0200

    Using templates

--- src/SudokuSolver/View/SudokuView.php
+++ src/SudokuSolver/View/SudokuView.php
@@ -8,6 +8,9 @@ use SudokuSolver\View\Template;
 class SudokuView
 {
 
+    /**
+     * @var Sudoku
+     */
     private $sudoku;
 
     /**
@@ -16,6 +19,9 @@ class SudokuView
     public function __construct(Sudoku $sudoku)
     {
         $this->sudoku = $sudoku;
+
+        $this->cellTemplate = Template::getTemplate('sudoku-cell-static');
+        $this->rowTemplate = Template::getTemplate('sudoku-row');
     }
 
     public function render()
@@ -24,14 +30,33 @@ class SudokuView
 
         // TODO: Break out html
         for ($row = 0; $row < 9; $row++) {
-            $html .= "<div>";
-            for ($col = 0; $col < 9; $col++) {
-                $value = $this->sudoku->getCell($row, $col);
-                $html .= "<span>$value</span>";
-            }
-            $html .= "</div>";
+            $html .= $this->getRowHtml($row);
         }
 
         return $html;
     }
+
+    protected function getRowHtml($row)
+    {
+        $rowHtml = '';
+        foreach ($this->sudoku->getRow($row) as $cell) {
+            $rowHtml .= $this->getCellHtml($cell);
+        }
+        return $this->rowTemplate->render(array('cells' => $rowHtml));
+    }
+
+    protected function getCellHtml($digit)
+    {
+        // Digit or empty
+        //$digit = $this->sudoku->isFilled($row, $col) ? $this->sudoku->getCell($row, $col) : '';
+        $digit = $digit ? $digit : '';
+
+        $opts = array(
+            'class' => '',
+            'digit' => $digit
+        );
+        return $this->cellTemplate->render($opts);
+    }
+
+
 }
