42de610cf229b7f115cf5b308ec95493c3171748
alcesleo <lagginglion@gmail.com>
2013-10-21 11:23:13 +0200

    Refine js

--- public/js/sudokuGrid.js
+++ public/js/sudokuGrid.js
@@ -1,16 +1,24 @@
-// Move to next input square when a valid number/nothing is entered,
-// prevent other characters from being entered.
 // TODO: Only include this on form-input page
-$('input.sudoku-cell').keypress(function (event) {
-    // Get char
-    var c = String.fromCharCode(event.which);
-    // If digit or space
-    if (/[1-9\ ]/.test(c)) {
-        // Move to next square
-        $(this).next('input.sudoku-cell')[0].focus();
-    } else {
-        // Don't allow
-        return false;
-    }
-    // TODO: Previous on backspace?
-});
+// TODO: 'Are you sure?' on clear-button
+(function () {
+    'use strict';
+
+    // Move to next input square when a valid number/nothing is entered,
+    // prevent other characters from being entered.
+    $('input.sudoku-cell').keypress(function (event) {
+        // Get char
+        var c = String.fromCharCode(event.which);
+        // If digit or space
+        if (/[1-9\ ]/.test(c)) {
+            // Move to next square
+            $(this).next('input.sudoku-cell')[0].focus();
+        } else {
+            // Don't allow
+            return false;
+        }
+        // TODO: Previous on backspace
+    });
+
+    // Make sure the grid stays square
+
+}());
