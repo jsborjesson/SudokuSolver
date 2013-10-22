91c75ac0acf08c12b873c12db6529a0b1a9b8570
alcesleo <lagginglion@gmail.com>
2013-10-22 14:12:06 +0200

    Better next-square

--- public/js/sudokuGrid.js
+++ public/js/sudokuGrid.js
@@ -1,24 +1,33 @@
 // TODO: Only include this on form-input page
+// FIXME: BUG: Will not enter number when shift-tabbed back to filled square
 // TODO: 'Are you sure?' on clear-button
 (function () {
     'use strict';
 
+    var sudokuCellSelector = 'input.sudoku-cell';
+
     // Move to next input square when a valid number/nothing is entered,
     // prevent other characters from being entered.
-    $('input.sudoku-cell').keypress(function (event) {
+    $(sudokuCellSelector).keypress(function (event) {
         // Get char
         var c = String.fromCharCode(event.which);
         // If digit or space
         if (/[1-9\ ]/.test(c)) {
+
+            // Get next input
+            var index = $(this).index(sudokuCellSelector);
+            var nextElem = $(sudokuCellSelector).eq(index + 1);
+
             // Move to next square
-            $(this).next('input.sudoku-cell')[0].focus();
+            if (nextElem != undefined) {
+                $(nextElem).focus();
+            }
+
         } else {
-            // Don't allow
+            // Don't allow inputting other chars
             return false;
         }
         // TODO: Previous on backspace
     });
-
-    // Make sure the grid stays square
 
 }());
