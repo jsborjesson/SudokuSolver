951b358626b46e082f4d668d7c301de7367193d0
alcesleo <lagginglion@gmail.com>
2013-10-23 16:08:33 +0200

    Draft of first solving step

--- src/SudokuSolver/Model/AiSolver.php
+++ src/SudokuSolver/Model/AiSolver.php
@@ -10,6 +10,10 @@ use SudokuSolver\Model\Sudoku;
  */
 class AiSolver implements SolverInterface
 {
+    const DEAD = 0;
+    const PROGRESS = 1;
+    const SOLVED = 2;
+
     public function __construct()
     {
         //code...
@@ -17,42 +21,75 @@ class AiSolver implements SolverInterface
 
     public function solve(Sudoku $sudoku)
     {
+        $status = self::DEAD;
 
-        $progress = false;
-        for ($index = 0; $index < 81; $index++) {
-            if ($this->solveCell($sudoku, $index)) {
-                $progress = true;
-            }
+        // Fast
+        $status = $this->fastScan($sudoku);
+        if ($status === self::SOLVED) {
+            return true;
         }
+        // DEAD or PROGRESS both need to move on to advanced scan
 
-        // If it has made progress, start the loop over
-        if ($progress) {
+        // Slow
+        $status = $this->advancedScan($sudoku);
+        if ($status === self::PROGRESS) {
+            // Fall down to faster methods
             $this->solve($sudoku);
-        } else {
-            // Else, run other methods
-            // $this->solveByRules($sudoku);
         }
+        // DEAD - advancedScan does not check for final solution
+
+        // If the method reaches this point, the logical steps have failed,
+        // and it needs to make an educated guess
+        $this->nextGuess($sudoku);
+
     }
 
     /**
      * Solves a cell if it only has one possible solution
      * @param  Sudoku $sudoku
      * @param  int $index
-     * @return bool         True if solved, false if not
+     * @return int          DEAD | PROGRESS | SOLVED
      */
-    private function solveCell(Sudoku $sudoku, $index)
+    private function fastScan(Sudoku $sudoku)
     {
-        $row = floor($index / 9);
-        $col = floor($index % 9);
 
-        $options = $sudoku->getOptionsForCell($row, $col);
+        do {
+            // if the algorithm has achieved anything
+            $makingProgress = false;
 
-        if (count($options) == 1) {
-            $sudoku->setCell($row, $col, $options[0]);
-            return true;
-        } else {
-            return false;
-        }
+            // assume it's solved, set this to false as soon as empty cell is found
+            $solved = true;
+
+            // TODO: $sudoku->forEachSquare(func($row, $col, $val))
+            // For each cell
+            for ($row = 0; $row < 9; $row++) {
+                for ($col = 0; $col < 9; $col++) {
+
+                    // Move on if already solved
+                    if ($sudoku->isFilled($row, $col)) {
+                        continue;
+                    } else {
+                        $solved = false;
+                    }
+
+                    $options = $sudoku->getOptionsForCell($row, $col);
+                    if (count($options) == 1) {
+                        $sudoku->setCell($row, $col, $options[0]);
+                        $makingProgress = true;
+                    }
+
+                }
+            }
+
+            // No cells left unsolved
+            if ($solved) {
+                return self::SOLVED;
+            } elseif ($makingProgress) {
+
+            }
+
+        } while ($makingProgress);
+
     }
 
     private function checkAndEnter($sudoku)
