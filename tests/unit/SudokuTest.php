40a87a6dab461a683c76275237362b90a0737c29
alcesleo <lagginglion@gmail.com>
2013-10-24 08:27:18 +0200

    Lots of new tests of Sudoku logic

--- tests/unit/SudokuTest.php
+++ tests/unit/SudokuTest.php
@@ -11,7 +11,7 @@ class SudokuTest extends \Codeception\TestCase\Test
 
     protected function _before()
     {
-        // Create test-sudoku
+        // Create valid test-sudoku used in many tests
         $this->sudoku = new Sudoku(
             array(
                 array(7, 2, 3, 0, 0, 0, 1, 5, 9),
@@ -40,17 +40,29 @@ class SudokuTest extends \Codeception\TestCase\Test
         $this->assertEquals(array(9,8,2,0,0,0,3,6,4), $this->sudoku->getColumn(8));
     }
 
-    public function testGetGroup()
+    public function testGetContainingGroup()
     {
         // top left
-        $this->assertEquals(array(7,2,3,6,0,0,8,0,0), $this->sudoku->getGroup(0, 0));
+        $this->assertEquals(array(7,2,3,6,0,0,8,0,0), $this->sudoku->getContainingGroup(0, 0));
 
         // middle
         $middle = array(6,5,4,2,0,7,9,3,1);
 
         // Both of these should give the middle group
-        $this->assertEquals($middle, $this->sudoku->getGroup(4, 4));
-        $this->assertEquals($middle, $this->sudoku->getGroup(3, 5));
+        $this->assertEquals($middle, $this->sudoku->getContainingGroup(4, 4));
+        $this->assertEquals($middle, $this->sudoku->getContainingGroup(3, 5));
+    }
+
+    public function testGetGroupByIndex()
+    {
+        // top left
+        $this->assertEquals(array(7,2,3,6,0,0,8,0,0), $this->sudoku->getGroupByIndex(0));
+
+        // middle
+        $this->assertEquals(array(6,5,4,2,0,7,9,3,1), $this->sudoku->getGroupByIndex(4));
+
+        // bottom left
+        $this->assertEquals(array(5,0,0,4,0,0,9,3,2), $this->sudoku->getGroupByIndex(6));
     }
 
     public function testIsFilled()
@@ -61,13 +73,40 @@ class SudokuTest extends \Codeception\TestCase\Test
         $this->assertTrue($this->sudoku->isFilled(8, 8));
     }
 
-    public function testIsValidWithValid()
+    public function testValidateValidSudoku()
     {
-        // The testing sudoku is valid
-        $this->assertTrue($this->sudoku->isValid());
+        // The testing sudoku is valid and should not throw
+        $this->sudoku->validate();
     }
 
-    public function testIsValidWithInvalidCell()
+    /**
+     * @expectedException         Exception
+     * @expectedExceptionMessage  Sudoku must be exactly 9x9 cells
+     */
+    public function testValidateNonSquareSudoku()
+    {
+        // Has invalid cell value
+        $inValidSudoku = new Sudoku(
+            array(
+                array(7, 2, 3, 0, 0, 5, 1, 5, 9),
+                array(6, 0, 0, 3, 0, 2, 0, 0, 8),
+                array(8, 0, 0, 0, 1, 0, 0, 0, 2),
+                array(0, 7, 0, 6, -5, 4, 0, 2, 0),
+                array(0, 0, 4, 2, 0, 7, 3, 0), // This row is too short!
+                array(0, 5, 0, 9, 3, 1, 0, 4, 0),
+                array(5, 0, 0, 0, 7, 0, 0, 0, 3),
+                array(4, 0, 0, 1, 0, 3, 0, 0, 6),
+                array(9, 3, 2, 0, 0, 0, 7, 1, 4)
+            )
+        );
+        $inValidSudoku->validate();
+    }
+
+    /**
+     * @expectedException         Exception
+     * @expectedExceptionMessage  Invalid cell value
+     */
+    public function testValidateWithInvalidCell()
     {
         // Has invalid cell value
         $inValidSudoku = new Sudoku(
@@ -83,10 +122,14 @@ class SudokuTest extends \Codeception\TestCase\Test
                 array(9, 3, 2, 0, 0, 0, 7, 1, 4)
             )
         );
-        $this->assertFalse($inValidSudoku->isValid());
+        $inValidSudoku->validate();
     }
 
-    public function testIsValidWithDuplicatedCell()
+    /**
+     * @expectedException         Exception
+     * @expectedExceptionMessage  Duplicate value in row 0
+     */
+    public function testValidateWithDuplicatedCell()
     {
         // Has duplicated digits in row 1
         $inValidSudoku = new Sudoku(
@@ -102,7 +145,7 @@ class SudokuTest extends \Codeception\TestCase\Test
                 array(9, 3, 2, 0, 0, 0, 7, 1, 4)
             )
         );
-        $this->assertFalse($inValidSudoku->isValid());
+        $inValidSudoku->validate();
     }
 
     public function testIsSolved()
@@ -125,5 +168,22 @@ class SudokuTest extends \Codeception\TestCase\Test
             )
         );
         $this->assertTrue($solvedSudoku->isSolved());
+    }
+
+    public function testClone()
+    {
+        // Make sure the clone does not reference the same array
+
+        // Create clone
+        $clone = clone($this->sudoku);
+
+        // Make sure they are the same
+        $this->assertEquals($clone->getRow(8), $this->sudoku->getRow(8));
+
+        // Change a cell
+        $clone->setCell(8, 0, 6); // 9 -> 6
+
+        // Make sure both have not changed
+        $this->assertNotEquals($clone->getRow(8), $this->sudoku->getRow(8));
     }
 }
