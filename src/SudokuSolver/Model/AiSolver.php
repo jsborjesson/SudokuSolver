59a382a7480c1ebc1e6673223594c56324aba226
alcesleo <lagginglion@gmail.com>
2013-10-27 18:01:27 +0100

    Backing up stupid methods before I delete them

--- src/SudokuSolver/Model/AiSolver.php
+++ src/SudokuSolver/Model/AiSolver.php
@@ -16,7 +16,7 @@ class AiSolver implements SolverInterface
      * @param  Sudoku $sudoku
      * @return bool
      */
-    public function solve(Sudoku $sudoku)
+    public function solve(Sudoku $sudoku, $deep = true)
     {
         // NOTE: Pay attention to the different meanings of the scanning-functions return values!
 
@@ -37,7 +37,9 @@ class AiSolver implements SolverInterface
 
         // If the method reaches this point, the logical steps have failed,
         // and it needs to make an educated guess
-        // $this->nextGuess($sudoku);
+        if ($deep) {
+            $this->gamblingScan($sudoku);
+        }
 
     }
 
@@ -56,7 +58,6 @@ class AiSolver implements SolverInterface
             // assume it's solved, set this to false as soon as empty cell is found
             $solved = true;
 
-            // TODO: $sudoku->forEachSquare(func($row, $col, $val))
             // For each cell
             for ($row = 0; $row < 9; $row++) {
                 for ($col = 0; $col < 9; $col++) {
@@ -89,6 +90,38 @@ class AiSolver implements SolverInterface
 
     }
 
+
+    private function gamblingScan(Sudoku $sudoku)
+    {
+        // Try cells with the fewest possible options
+        for ($maximumOptions = 2; $maximumOptions < 9; $maximumOptions++) {
+
+            for ($row = 0; $row < 9; $row++) {
+                for ($col = 0; $col < 9; $col++) {
+
+                    // Move on if already solved
+                    if ($sudoku->isFilled($row, $col)) {
+                        continue;
+                    }
+
+                    $options = $sudoku->getOptionsForCell($row, $col);
+                    if (count($options) <= $maximumOptions) {
+
+                        foreach ($options as $option) {
+                            $sudoku->setCell($row, $col, $option);
+
+                            if ($this->solve($sudoku, false)) {
+                                return true;
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+
     /**
      * @param  Sudoku $sudoku
      * @return bool           If **made progress**
@@ -96,25 +129,80 @@ class AiSolver implements SolverInterface
     private function advancedScan(Sudoku $sudoku)
     {
 
+            // Check all units
+                // Check all options
+                    // Check all indices
+                        // Count if option is valid at current unit/index
+                    // If one option is only valid at one index - insert it
+        return;
+
+        // Check rows
+        if ($this->checkUnit(
+            $sudoku,
+            function ($unit, $index) use ($sudoku) {
+                return $sudoku->getOptionsForCell($unit, $index);
+            },
+            function ($unit, $index, $value) use ($sudoku) {
+                $sudoku->setCell($unit, $index, $value);
+            }
+        )) {
+            return true;
+        }
+
+        // Check cols
+        if ($this->checkUnit(
+            $sudoku,
+            function ($unit, $index) use ($sudoku) {
+                return $sudoku->getOptionsForCell($index, $unit);
+            },
+            function ($unit, $index, $value) use ($sudoku) {
+                $sudoku->setCell($index, $unit, $value);
+            }
+        )) {
+            return true;
+        }
+
+        // Check groups
+        // Check rows
+        if ($this->checkUnit(
+            $sudoku,
+            function ($unit, $index) use ($sudoku) {
+                $row = floor($index / 3);
+                $col = floor($index % 3);
+
+                return $sudoku->getOptionsForCell($unit, $index);
+            },
+            function ($unit, $index, $value) use ($sudoku) {
+                $sudoku->setCell($unit, $index, $value);
+            }
+        )) {
+            return true;
+        }
+    }
+
+    private function checkUnit(Sudoku $sudoku, callable $getOptions, callable $setCellInUnit)
+    {
         // For all units
-        for ($row = 0; $row < 9; $row++) {
+        for ($unit = 0; $unit < 9; $unit++) {
 
             // All possible options
             for ($option = 0; $option < 9; $option++) {
 
-                // Count the times that option is valid in this row
+                // Count the times that option is valid in this unit
                 $times = 0;
 
-                $lastCol = -1;
+                $lastIndex = -1;
 
-                for ($col = 0; $col < 9; $col++) {
+                for ($index = 0; $index < 9; $index++) {
 
                     // If option is valid in this cell
 
-                    $options = $sudoku->getOptionsForCell($row, $col);
+                    // CALLBACK
+                    $options = $getOptions($unit, $index);
+
                     if (in_array($option, $options)) {
                         $times++;
-                        $lastCol = $col;
+                        $lastIndex = $index;
 
                         // Break? on 2?
                     }
@@ -122,7 +210,8 @@ class AiSolver implements SolverInterface
 
                 // If the option can only go in one place - insert it there
                 if ($times == 1) {
-                    $sudoku->setCell($row, $lastCol, $option);
+                    // THIS
+                    $setCellInUnit($unit, $index, $option);
                     return true;
                 }
             }
